776a61ffcff4a1dc2de08e438554e422cb1e80ba
Chico Almeida <francisco.almeida@outlook.com>
2016-09-15 21:44:55 -0300

    add configuration for WebApplication condition

--- src/main/java/com/justgifit/JustGifItAutoConfiguration.java
+++ src/main/java/com/justgifit/JustGifItAutoConfiguration.java
@@ -5,10 +5,22 @@ import com.justgifit.services.GifEncoderService;
 import com.justgifit.services.VideoDecoderService;
 import com.madgag.gif.fmsware.AnimatedGifEncoder;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.filter.HttpPutFormContentFilter;
+import org.springframework.web.filter.RequestContextFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.io.File;
 
 /**
  * Created by francisco.almeida on 9/15/16.
@@ -17,19 +29,79 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass({FFmpegFrameGrabber.class, AnimatedGifEncoder.class})
 public class JustGifItAutoConfiguration {
 
+    @Value("${multipart.location}/gif/")
+    private String gifLocation;
+
+    @Bean
+    @ConditionalOnProperty(prefix = "com.justgifit", name = "create-result-dir")
+    public Boolean createResultDir() {
+        File gifFolder = new File(gifLocation);
+        if (!gifFolder.exists()) {
+            gifFolder.mkdir();
+        }
+        return true;
+    }
+
     @Bean
     @ConditionalOnMissingBean(VideoDecoderService.class)
-    public VideoDecoderService videoDecoderService(){
+    public VideoDecoderService videoDecoderService() {
         return new VideoDecoderService();
     }
+
     @Bean
     @ConditionalOnMissingBean(GifEncoderService.class)
-    public GifEncoderService gifEncoderService(){
+    public GifEncoderService gifEncoderService() {
         return new GifEncoderService();
     }
+
     @Bean
     @ConditionalOnMissingBean(ConverterService.class)
-    public ConverterService converterService(){
+    public ConverterService converterService() {
         return new ConverterService();
     }
+
+    @Configuration
+    @ConditionalOnWebApplication
+    public static class WebConfiguration {
+        @Value("${multipart.location}/gif/")
+        private String gifLocation;
+
+        @Bean
+        @ConditionalOnProperty(prefix = "com.justfgitit", name = "optimize")
+        public FilterRegistrationBean deRegisterHiddenHttpMethodFilter(HiddenHttpMethodFilter filter) {
+            FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+            bean.setEnabled(false);
+            return bean;
+        }
+
+        @Bean
+        @ConditionalOnProperty(prefix = "com.justfgitit", name = "optimize")
+        public FilterRegistrationBean deRegisterHttpPutFormContentFilter(HttpPutFormContentFilter filter) {
+            FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+            bean.setEnabled(false);
+            return bean;
+        }
+
+        @Bean
+        @ConditionalOnProperty(prefix = "com.justfgitit", name = "optimize")
+        public FilterRegistrationBean deRegisterRequestContextFilter(RequestContextFilter filter) {
+            FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+            bean.setEnabled(false);
+            return bean;
+        }
+
+        @Bean
+        @ConditionalOnProperty(prefix = "com.justfgitit", name = "optimize")
+        public WebMvcConfigurer webMvcConfigurer() {
+            return new WebMvcConfigurerAdapter() {
+                @Override
+                public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                    registry.addResourceHandler("/gif/**")
+                            .addResourceLocations("file:" + gifLocation);
+                    super.addResourceHandlers(registry);
+                }
+            };
+        }
+    }
+
 }
